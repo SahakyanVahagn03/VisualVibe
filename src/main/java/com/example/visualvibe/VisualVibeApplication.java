fd16189e469abf0557b24d53d0cca0d408b8b7f2
Vahagn Sahakyan <vahagnsahakyan53@gmail.com>
2024-06-09 22:43:16 +0400

    first commit for this project

---
+++ src/main/java/com/example/visualvibe/VisualVibeApplication.java
@@ -0,0 +1,13 @@
+package com.example.visualvibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualVibeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualVibeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/visualvibe/VisualVibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visualvibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualVibeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
